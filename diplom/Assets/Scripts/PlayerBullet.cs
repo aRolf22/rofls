50450df767e246f109206deb598866a71941282d
PashaEVD123 <evdopavel1757@gmail.com>
2025-03-25 18:47:40 +0300

    ну типо ограничил полет пули по времени
    Говорят, за перенос в Иерусалим посольства США в Израиле Трампу бесплатно сделают обрезание. anekdotov.net, Вопрос, кто – евреи или арабы?

--- diplom/Assets/Scripts/PlayerBullet.cs
+++ diplom/Assets/Scripts/PlayerBullet.cs
@@ -2,46 +2,51 @@ using UnityEngine;
 
 public class PlayerBullet : MonoBehaviour
 {
-
     public float speed = 7.5f;
     public Rigidbody2D theRB;
 
     public GameObject impactEffect;
-
     public int damageToGive = 50;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Время жизни пули
+    public float lifetime = 2.0f; // Время в секундах
+    private float timeAlive;
+
     void Start()
     {
-        
+        timeAlive = 0f; // Инициализация времени жизни
     }
 
-    // Update is called once per frame
     void Update()
     {
         theRB.linearVelocity = transform.right * speed;
+
+        // Увеличиваем время жизни
+        timeAlive += Time.deltaTime;
+
+        // Если время жизни превышает заданное, уничтожаем пулю
+        if (timeAlive >= lifetime)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        Instantiate(impactEffect, transform.position, transform.rotation); // Спавним эффект удара в месте попадания пули
-        
-            Destroy(gameObject); // Уничтожаем саму пулю
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+        AudioManager.instance.PlaySFX(4);
 
-            AudioManager.instance.PlaySFX(4);    // звук Impact
+        if (other.tag == "Enemy")
+        {
+            other.GetComponent<EnemyController>().DamageEnemy(damageToGive);
+        }
 
-            if (other.tag == "Enemy") // "Если у объекта, в который попала пуля, есть тэг "Enemy"
-            {
-                other.GetComponent<EnemyController>().DamageEnemy(damageToGive); // Вызываем у EnemyController метод DamageEnemy
-            }
-
-            if (other.tag == "Boss")
-            {
-                BossController.instance.TakeDamage(damageToGive);
-
-                Instantiate(BossController.instance.hitEffect, transform.position, transform.rotation);
-            }
-
+        if (other.tag == "Boss")
+        {
+            BossController.instance.TakeDamage(damageToGive);
+            Instantiate(BossController.instance.hitEffect, transform.position, transform.rotation);
+        }
     }
 
     void OnBecameInvisible() 

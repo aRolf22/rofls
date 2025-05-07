82ab28f11ab0a6919bf6530e4e4997b2331c9ac0
RaymosDev <raycastm22@gmail.com>
2025-05-08 01:57:09 +0300

    Пока только assets из 7.5

---
+++ diplom/Assets/Scripts/Misc/Settings.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public static class Settings
+{
+
+    #region ROOM SETTINGS
+    public const float fadeInTime = 0.5f; // time to fade in the room
+    public const int maxChildCorridors = 3; // Max number of child corridors leading from a room. - maximum should be 3 although this is not recommended since it can cause the dungeon building to fail since the rooms are more likely to not fit together;
+    public const float doorUnlockDelay = 1f;
+    #endregion
+
+}

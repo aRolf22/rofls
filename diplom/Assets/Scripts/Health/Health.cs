ba54f68a98141700adcd21caf5535ba162177680
PashaEVD123 <evdopavel1757@gmail.com>
2025-05-09 05:24:59 +0300

    чет анимация в game слегка не так идет

---
+++ diplom/Assets/Scripts/Health/Health.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Health : MonoBehaviour
+{
+    private int startingHealth;
+    private int currentHealth;
+
+    /// <summary>
+    /// Set starting health 
+    /// </summary>
+    public void SetStartingHealth(int startingHealth)
+    {
+        this.startingHealth = startingHealth;
+        currentHealth = startingHealth;
+    }
+
+    /// <summary>
+    /// Get the starting health
+    /// </summary>
+    public int GetStartingHealth()
+    {
+        return startingHealth;
+    }
+}
